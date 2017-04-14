3dbdbfb9ed0f3cbfd35480e1d0fbb8e5d8091b80
Ismail <eng_elec2009@yahoo.com>
2017-04-14 13:59:38 +0200

    forcing fullscreen view

--- _protected/backend/views/layouts/main.php
+++ _protected/backend/views/layouts/main.php
@@ -1,9 +1,6 @@
 <?php
 use backend\assets\AppAsset;
-use frontend\widgets\Alert;
 use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 
 
@@ -31,7 +28,7 @@ AppAsset::register($this);
     <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/droid-arabic-kufi" type="text/css"/>
     <?php $this->head() ?>
   </head>
-  <body class="skin-blue sidebar-mini">
+  <body class="skin-blue sidebar-mini sidebar-collapse">
       <?php $this->beginBody() ?>
     <div class="wrapper">
 
@@ -44,7 +41,7 @@ AppAsset::register($this);
           <span class="logo-lg"><b>Alraheem</b>Admin</span>
         </a>
         <!-- Header Navbar: style can be found in header.less -->
-        <nav class="navbar navbar-static-top" role="navigation">
+        <nav class="navbar navbar-static-top margin-50" role="navigation">
           <!-- Sidebar toggle button-->
           <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
             <span class="sr-only">Toggle navigation</span>
@@ -96,7 +93,7 @@ AppAsset::register($this);
       <div class="content-wrapper">
         <!-- Content Header (Page header) -->
         <section class="content-header">
-            <h1 class="<?php if(Yii::$app->params['sidebar'] == 'fullScreen') echo 'margin-50' ?>">
+            <h1 class="margin-50">
             <?= Html::encode($this->title) ?>
           </h1>
           <?= Breadcrumbs::widget([
@@ -105,7 +102,7 @@ AppAsset::register($this);
         </section>
 
         <!-- Main content -->
-        <section class="content <?php if(Yii::$app->params['sidebar'] == 'fullScreen') echo 'margin-50' ?>">
+        <section class="content margin-50">
           <div class="box">
                 <div class="box-body">
                     <?= $content ?>
